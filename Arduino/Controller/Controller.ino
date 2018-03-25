ab3f4980fe747343820c224e7f0b938ff7b9f933
Marlier <norman.marlier@gmail.com>
2018-03-25 21:35:12 +0200

    The robot can be moved by the serial link

--- Arduino/Controller/Controller.ino
+++ Arduino/Controller/Controller.ino
@@ -1,11 +1,17 @@
 #include <Servo.h>
 #include "data_structure.h"
 
+// Volatile variables
+volatile bool emergency_state = false; 
+
+// Stream buffer
+String read_string;
 
 // Servomotors of the robot
 Servo servo_1;
 Servo servo_2;
-Servo servo_3;
+//Servo servo_3;
+Servo gripper;
 
 // Angle variable
 float angle = 0;
@@ -16,19 +22,33 @@ Position home_pos = {0, 90, 0};
 // Robot structure
 Robot robot = {home_pos, CLOSE};
 
+/* 
+ *  Interrupt routine    
+ *
+ */
+void emergency_button()
+{
+  emergency_state = !emergency_state;
+}
+
 void setup() 
 {
   // Initialize the serial link
   Serial.begin(9600);
 
+  // Interrupt routine
+  attachInterrupt(0, emergency_button, CHANGE);
+
   // Attach the servos and go to home pos
-  servo_1.attach(2, 1000, 2000);
-  servo_2.attach(4, 1000, 2000);
-  servo_3.attach(6, 1000, 2000);
+  servo_1.attach(3, 1000, 2000);
+  servo_2.attach(5, 1000, 2000);
+  //servo_3.attach(7, 1000, 2000);
+  gripper.attach(8);
   
   servo_1.write(home_pos.angle_1);
   servo_2.write(home_pos.angle_2);
-  servo_3.write(home_pos.angle_3);
+  //servo_3.write(home_pos.angle_3);
+  gripper.write(robot.gripper_state);
 
   // Wait 3s
   delay(3000);
@@ -37,17 +57,41 @@ void setup()
 void loop() 
 {
 
-  if (angle == 180) angle = 0;
-  else angle += 20;
-  Serial.print(angle);
-  Serial.println("\n");
-  robot.pos.angle_1 = angle;
-  robot.pos.angle_2 = angle;
-  robot.pos.angle_3 = angle;
-  servo_1.write(robot.pos.angle_1);
-  servo_2.write(robot.pos.angle_2);
-  servo_3.write(robot.pos.angle_3);
-  delay(500);
+  // Check for communication
+  while (Serial.available())
+  {
+    // read the incoming byte:
+    char c = Serial.read();
+    read_string += c;
+    delay(2);     
+  }
+
+  // Movin instructions
+  if(read_string.length() > 0)
+  {
+    //convert readString into a number
+    int n = read_string.toInt();
+
+    // Check its value
+    if (n <= 180 || n >= 0)
+    {
+      if (!emergency_state)
+      {
+        Serial.print("writing Angle: ");
+        Serial.println(n);
+        servo_1.write(n);
+        servo_2.write(n);
+        if (n > 90) robot.gripper_state = OPEN;
+        else robot.gripper_state = CLOSE;
+        gripper.write(robot.gripper_state);
+        
+      }
+
+      read_string ="";
+      
+    }
+  }
+  
   
 
 }
