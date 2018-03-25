2d9ace8d2438258a6c9f670c1b72fcaa4892c971
Marlier <norman.marlier@gmail.com>
2018-03-25 21:37:39 +0200

    ROS interface with Arduin controller Not stable

---
+++ src/InterfaceController.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+"""
+
+docstring
+
+"""
+
+# Import module
+#import rospy
+#from std_msgs.msg import String
+
+
+# Meta data
+__author__ = 'Norman Marlier'
+__copyright__ = 'Copyright 2018'
+__credits__ = ['Norman Marlier']
+__maintainer__ = "Norman Marlier"
+__email__ = "norman.marlier@student.uliege.be"
+__status__ = "Prototype"
+
+
+
+
+
+
+
+
