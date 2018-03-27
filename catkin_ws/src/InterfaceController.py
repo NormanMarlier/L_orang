b7b9061275084a8c12c9594aad91065e2cfb519a
ros <norman.marlier@gmail.com>
2018-03-27 23:05:32 +0200

    Remove Empty msg

--- catkin_ws/src/InterfaceController.py
+++ catkin_ws/src/InterfaceController.py
@@ -21,13 +21,12 @@ __status__ = "Prototype"
 
 
 def talker():
-    Empty msg;
     pub = rospy.Publisher('lorang', Empty, queue_size=10)
     rospy.init_node('talker', anonymous=True)
     rate = rospy.Rate(10) # 10hz
     while not rospy.is_shutdown():
         rospy.loginfo('Change the state of the gripper')
-        pub.publish(msg)
+        pub.publish()
         rate.sleep()
 
 if __name__ == '__main__':
