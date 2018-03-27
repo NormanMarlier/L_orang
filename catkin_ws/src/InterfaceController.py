4f5ab85f62e9f43a08d4fdd054a08942f8e38805
Marlier <norman.marlier@gmail.com>
2018-03-27 23:00:15 +0200

    Add ROS publisher

--- catkin_ws/src/InterfaceController.py
+++ catkin_ws/src/InterfaceController.py
@@ -7,8 +7,8 @@ docstring
 """
 
 # Import module
-#import rospy
-#from std_msgs.msg import String
+import rospy
+from std_msgs.msg import Empty
 
 
 # Meta data
@@ -20,9 +20,21 @@ __email__ = "norman.marlier@student.uliege.be"
 __status__ = "Prototype"
 
 
+def talker():
+    Empty msg;
+    pub = rospy.Publisher('lorang', Empty, queue_size=10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10) # 10hz
+    while not rospy.is_shutdown():
+        rospy.loginfo('Change the state of the gripper')
+        pub.publish(msg)
+        rate.sleep()
 
-
-
+if __name__ == '__main__':
+    try:
+        talker()
+    except rospy.ROSInterruptException:
+        pass
 
 
 
