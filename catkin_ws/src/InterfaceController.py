3bb5123572e6cfd9651a5bddd904c55a82a8e153
NormanMarlier <norman.marlier@gmail.com>
2018-03-28 21:03:59 +0200

    Clean the code

--- catkin_ws/src/InterfaceController.py
+++ catkin_ws/src/InterfaceController.py
@@ -23,13 +23,14 @@ __status__ = "Prototype"
 def talker():
     pub = rospy.Publisher('lorang', Empty, queue_size=10)
     rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
+    rate = rospy.Rate(0.5) # 10hz
     while not rospy.is_shutdown():
         rospy.loginfo('Change the state of the gripper')
         pub.publish()
         rate.sleep()
 
 if __name__ == '__main__':
+  	
     try:
         talker()
     except rospy.ROSInterruptException:
