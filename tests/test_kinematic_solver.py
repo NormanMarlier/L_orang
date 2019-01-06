0e30180797660da99436fd29b4201c6b94b6edaf
Marlier <norman.marlier@gmail.com>
2019-01-06 22:44:03 +0100

    Modify tests for kinematic solver

--- tests/test_kinematic_solver.py
+++ tests/test_kinematic_solver.py
@@ -12,40 +12,57 @@ class TestRRRSolver:
     def test_init_solver_1(self):
         # If there is no robot l1 = l2 = l3 = 0
         with pytest.raises(ValueError):
-            self.solver = RRRSolver(0, 0, 0, [0, math.pi], [0, math.pi], [0, math.pi])
+            self.solver = RRRSolver(0, 0, 0, [0, 180], [0, 180], [0, 180])
 
     def test_init_solver_2(self):
         # If there is no negative length
         with pytest.raises(ValueError):
-            self.solver = RRRSolver(-1, 0, 0, [0, math.pi], [0, math.pi], [0, math.pi])
+            self.solver = RRRSolver(0, -1, 0, [0, 180], [0, 180], [0, 180])
+
+    def test_init_solver_3(self):
+        # If there is no robot l2 = l3 = 0
+        with pytest.raises(ValueError):
+            self.solver = RRRSolver(1, 0, 0, [0, 180], [0, 180], [0, 180])
 
     def test_check_angle_1(self):
-        self.solver = RRRSolver(1, 1, 1, [-0.1, 3.14], [-0.1, 3.14], [-0.1, 3.14])
+        self.solver = RRRSolver(1, 1, 1, [-10, 360], [-10, 360], [-10, 360])
 
-        result = self.solver.check_angle([-0.2, 0, 0])
+        result = self.solver.check_angle([-20, 0, 0])
         assert result is False
 
     def test_check_angle_2(self):
-        self.solver = RRRSolver(1, 1, 1, [-0.1, 3.14], [-0.1, 3.14], [-0.1, 3.14])
+        self.solver = RRRSolver(1, 1, 1, [-10, 360], [-10, 360], [-10, 360])
 
-        result = self.solver.check_angle([-0, 4, 0])
+        result = self.solver.check_angle([-0, 400, 0])
         assert result is False
 
     def test_check_angle_3(self):
-        self.solver = RRRSolver(1, 1, 1, [-0.1, 3.14], [-0.1, 3.14], [-0.1, 3.14])
+        self.solver = RRRSolver(1, 1, 1, [-10, 360], [-10, 360], [-10, 360])
 
-        result = self.solver.check_angle([-2, -2, -2])
+        result = self.solver.check_angle([-20, -20, -20])
         assert result is False
 
     def test_check_angle_4(self):
-        self.solver = RRRSolver(1, 1, 1, [-0.1, 3.14], [-0.1, 3.14], [-0.1, 3.14])
+        self.solver = RRRSolver(1, 1, 1, [-10, 360], [-10, 360], [-10, 360])
 
         result = self.solver.check_angle([0, 0, 0])
         assert result is True
 
+    def test_calibration_1(self):
+        self.solver = RRRSolver(1, 1, 1, [-10, 360], [-10, 360], [-10, 360])
+
+        with pytest.raises(ValueError):
+            self.solver.calibration([0, 0, 0], [1, 1, 0])
+
+    def test_calibration_2(self):
+        self.solver = RRRSolver(1, 1, 1, [-10, 360], [-10, 360], [-10, 360])
+
+        with pytest.raises(ValueError):
+            self.solver.calibration([0, 0, 0], [0, 0, -1])
+
     def test_fkine(self):
         # Test one case that is good
-        self.solver = RRRSolver(1, 1, 1, [-0.1, 3.14], [-0.1, 3.14], [-0.1, 3.14])
+        self.solver = RRRSolver(1, 1, 1, [-10, 360], [-10, 360], [-10, 360])
         # Init pose
         init_pose_angle = [0, math.pi / 4, 0]
         # fkine
@@ -54,7 +71,7 @@ class TestRRRSolver:
 
     def test_ikine(self):
         # Test one case that is good
-        self.solver = RRRSolver(1, 1, 1, [-0.1, 3.14], [-0.1, 3.14], [-0.1, 3.14])
+        self.solver = RRRSolver(1, 1, 1, [-10, 360], [-10, 360], [-10, 360])
         # Init pose
         init_pose_angle = (0., math.pi / 4, 0.)
         end_pose_angle = self.solver.ikine([1.7071067811865475, 0.0, 1.7071067811865475])
